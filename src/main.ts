80e6f3b3dac88166531ad0cffec4c6a33eed16a3
Collin Schindler <collin_schindler@special-lite.com>
2026-01-16 12:05:24 -0600

    Common Material types with various performance differences
    Also, super useful example for showing HTML on top of a three.js object. Particularly useful when showing door marks

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,19 @@
 import './style.css'
 import * as THREE from 'three'
-import {OrbitControls} from 'three/addons/controls/OrbitControls.js'  // Mouse controls
-import Stats from 'three/addons/libs/stats.module.js' // Stats panel
-import {GUI} from 'dat.gui' // GUI for objects in scene?
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import Stats from 'three/addons/libs/stats.module.js'
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
-const sceneA = new THREE.Scene()
-const sceneB = new THREE.Scene()
+// scene.environment = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
 
-scene.background = new THREE.Color(0x123456)
-sceneA.background = new THREE.TextureLoader().load('https://sbcode.net/img/grid.png')
-sceneB.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/')
-.load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
+const gridHelper = new THREE.GridHelper()
+scene.add(gridHelper)
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 1.5
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
+camera.position.set(-1, 4, 2.5)
 
-const canvas = document.getElementById("threeJsCanvas") as HTMLCanvasElement
-const renderer = new THREE.WebGLRenderer({ canvas: canvas })
+const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
@@ -25,84 +21,114 @@ window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
-  renderer.render(scene, camera) // used to manually rerender when the screen size changes
 })
 
-const geometry = new THREE.BoxGeometry(1, 1.5, .1)
-const hardware = new THREE.SphereGeometry(.05, 6, 3)
-hardware.translate(-.4, -.03, 0)
-const material = new THREE.MeshNormalMaterial({ wireframe: true })
-const handleMaterial = new THREE.MeshNormalMaterial({ transparent: true, opacity: .5,})
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
 
-const cube = new THREE.Mesh(geometry, material)
-const handle = new THREE.Mesh(hardware, handleMaterial)
-scene.add(cube)
-scene.add(handle)
+const light = new THREE.DirectionalLight(undefined, Math.PI)
+light.position.set(1, 1, 1)
+scene.add(light)
 
-// Adding stats panel
+const data = { color: 0x00ff00, labelsVisible: true }
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(10, 10), new THREE.MeshBasicMaterial({ color: 0x00ff00 }))
+plane.rotation.x = -Math.PI / 2
+plane.visible = false
+scene.add(plane)
+
+const geometry = new THREE.IcosahedronGeometry(1, 1)
+
+const meshes = [
+  new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: data.color })),
+  new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({ flatShading: true })),
+  new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({ color: data.color, flatShading: true })),
+  new THREE.Mesh(geometry, new THREE.MeshStandardMaterial({ color: data.color, flatShading: true })),
+]
+
+meshes[0].position.set(-3, 1, 0)
+meshes[1].position.set(-1, 1, 0)
+meshes[2].position.set(1, 1, 0)
+meshes[3].position.set(3, 1, 0)
+
+scene.add(...meshes)
+
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
-// Adding object gui
 const gui = new GUI()
 
-// // Adding Folder for GUI
-const cubeFolder = gui.addFolder("Door Orientation")
-cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2)
+const meshBasicMaterialFolder = gui.addFolder('MeshBasicMaterial')
+meshBasicMaterialFolder.addColor(data, 'color').onChange(() => {
+  ;(meshes[0].material as THREE.MeshBasicMaterial).color.set(data.color)
+})
+meshBasicMaterialFolder.add(meshes[0].material, 'wireframe')
+meshBasicMaterialFolder.open()
 
-// // Adding camera folder for GUI
-const cameraFolder = gui.addFolder("Camera Zoom")
-cameraFolder.add(camera.position, "z", 1.5, 5)
+const meshNormalMaterialFolder = gui.addFolder('MeshNormalMaterial')
+meshNormalMaterialFolder.add(meshes[1].material as THREE.MeshNormalMaterial, 'flatShading').onChange(() => {
+  meshes[1].material.needsUpdate = true
+})
+meshNormalMaterialFolder.add(meshes[1].material, 'wireframe')
+meshNormalMaterialFolder.open()
 
-// // Adding size sliders for cube
-const cubeSizingFolder = gui.addFolder("Door Sizing")
-cubeSizingFolder.add(cube.scale, "x", 1, 1.96)
-cubeSizingFolder.add(cube.scale, "y", 1, 2.44)
-cubeSizingFolder.add(cube.scale, "z", .1, .175)
+const meshPhongMaterialFolder = gui.addFolder('MeshPhongMaterial')
+meshPhongMaterialFolder.addColor(data, 'color').onChange(() => {
+  ;(meshes[2].material as THREE.MeshPhongMaterial).color.set(data.color)
+})
+meshPhongMaterialFolder.add(meshes[2].material as THREE.MeshPhongMaterial, 'flatShading').onChange(() => {
+  meshes[2].material.needsUpdate = true
+})
+meshPhongMaterialFolder.add(meshes[2].material, 'wireframe')
+meshPhongMaterialFolder.open()
 
-// // Adding hardware position sliders
-const hardwareFolder = gui.addFolder("Hardware Position")
-hardwareFolder.add(handle.position, "x", 0, .8)
-hardwareFolder.add(handle.position, "y", -.4, .4)
+const meshStandardMaterialFolder = gui.addFolder('MeshStandardMaterial')
+meshStandardMaterialFolder.addColor(data, 'color').onChange(() => {
+  ;(meshes[3].material as THREE.MeshStandardMaterial).color.set(data.color)
+})
+meshStandardMaterialFolder.add(meshes[3].material as THREE.MeshStandardMaterial, 'flatShading').onChange(() => {
+  meshes[3].material.needsUpdate = true
+})
+meshStandardMaterialFolder.add(meshes[3].material, 'wireframe')
+meshStandardMaterialFolder.open()
 
-let activeScene = scene
-const setScene = {
-  scene: function () {
-    activeScene = scene
-  },
-  sceneA: function () {
-    activeScene = sceneA
-  },
-  sceneB: function () {
-    activeScene = sceneB
+const lightFolder = gui.addFolder('Light')
+lightFolder.add(light, 'visible')
+lightFolder.open()
+
+const gridFolder = gui.addFolder('Grid')
+gridFolder.add(gridHelper, 'visible')
+gridFolder.open()
+
+const labelsFolder = gui.addFolder('Labels')
+labelsFolder.add(data, 'labelsVisible')
+labelsFolder.open()
+
+const labels = document.querySelectorAll<HTMLDivElement>('.label')
+
+let x, y
+const v = new THREE.Vector3()
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  controls.update()
+
+  for (let i = 0; i < 4; i++) {
+    v.copy(meshes[i].position)
+    v.project(camera)
+
+    x = ((1 + v.x) / 2) * innerWidth - 50
+    y = ((1 - v.y) / 2) * innerHeight
+
+    labels[i].style.left = x + 'px'
+    labels[i].style.top = y + 'px'
+    labels[i].style.display = data.labelsVisible ? 'block' : 'none'
   }
+
+  renderer.render(scene, camera)
+
+  stats.update()
 }
 
-// Adding mouse controls
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.addEventListener('change', function () {
-  renderer.render(activeScene, camera)
-})
-
-const sceneSwapFolder = gui.addFolder("Scene Swap")
-sceneSwapFolder.add(setScene, 'scene').name('Door Only')
-sceneSwapFolder.add(setScene, 'sceneA').name('Hardware Only')
-sceneSwapFolder.add(setScene, 'sceneB').name('Door and Hardware')
-
-// Adding delta time to accomodate different screen refresh rates
-// const clock = new THREE.Clock()
-// let delta
-
-// function animate() {
-//   requestAnimationFrame(animate)
-
-//   delta = clock.getDelta()
-
-//   renderer.render(activeScene, camera)
-
-//   stats.update()
-// }
-
-renderer.render(activeScene, camera)
+animate()
